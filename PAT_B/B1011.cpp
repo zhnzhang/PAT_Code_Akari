055c719f5a782a0c3987c26c6bb4505eea3c369f
FunamiYui <46393174+FunamiYui@users.noreply.github.com>
2019-01-05 22:37:07 +0800

    Update B1011.cpp

--- PAT_B/B1011.cpp
+++ PAT_B/B1011.cpp
@@ -16,3 +16,25 @@ int main(){
   }
   return 0;
 }
+
+
+
+
+/*
+#include <cstdio>                     //这里单点测试，输出是按照输出样例这样一整组还是说可以一组输入一组输出？这关系到写法
+int main() {                          //下面的是书上给的代码
+  int T, tcase = 1;
+  long long a, b, c;
+  scanf("%d", &T);
+  while(T--) {
+    scanf("%lld%lld%lld", &a, &b, &c);
+    if(a + b > c) {
+      printf("Case #%d: true\n", tcase++);
+    }
+    else {
+      printf("Case #%d: false\n", tcase++);
+    }
+  }
+  return 0;
+}
+*/
