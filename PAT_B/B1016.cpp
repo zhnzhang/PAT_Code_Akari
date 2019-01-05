1596a04143cb5a0b09beb829eb69e879871bd0f8
FunamiYui <46393174+FunamiYui@users.noreply.github.com>
2019-01-05 22:36:17 +0800

    Update B1016.cpp

--- PAT_B/B1016.cpp
+++ PAT_B/B1016.cpp
@@ -31,3 +31,27 @@ int main() {
   printf("%lld\n", Apart + Bpart);
   return 0;
 }
+
+
+/*
+#include <cstdio>
+int main() {
+  long long a, b, da, db;
+  scanf("%lld%lld%lld%lld", &a, &da, &b, &db);
+  long long pa = 0, pb = 0;
+  while(a != 0) {                 //枚举a的每一位
+    if(a % 10 == da) {            //如果当前位为da,给pa增加一位da
+      pa = pa * 10 + da;
+      a = a / 10;
+    }
+  }
+  while(b != 0) {                 //枚举b的每一位
+    if(b % 10 == db) {            //如果当前位为db,给pb增加一位db
+      pb = pb * 10 + db;
+      b = b / 10;
+    }
+  }
+  printf("%lld\n", pa + pb);
+  return 0;
+}
+*/
