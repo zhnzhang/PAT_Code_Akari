0f0c08e64223e421374b6cca00490be1ea0598c4
FunamiYui <46393174+FunamiYui@users.noreply.github.com>
2019-01-11 22:57:29 +0800

    Create B1036.cpp

---
+++ PAT_B/B1036.cpp
@@ -0,0 +1,35 @@
+//example
+//输出由三部分组成，第1行，第2～row-1行，第row行
+//重点说下2～row-1行，先输出一个a再输出col-2个空格，再输出一个a
+//技巧！：整数除以2进行四舍五入的操作可以通过判断它是否是奇数来解决，以避免浮点数的介入，省的麻烦。
+//当列数col是奇数时，行数row就是col / 2 + 1；当列数col是偶数时，行数row就是col / 2
+
+#include <cstdio>
+int main() {
+  int row, col; //行、列
+  char c;
+  scanf("%d %c", &col, &c);   //涉及到字符的输入，输入格式的空格最好照常大，以免出现违背正常逻辑的错误
+  if(col % 2 == 1)
+    row = col / 2 + 1;
+  else
+    row = col / 2;
+  //第一行
+  for(int i = 0; i < col; i++) {
+    printf("%c", c);
+  }
+  printf("\n");
+  //2~row-1行
+  for(int i = 0; i < row - 2; i++) {
+    printf("%c", c);
+    for(int j = 0; j < col - 2; j++) {
+      printf(" ");
+    }
+    printf("%c\n", c);
+  }
+  //最后一行
+  for(int i = 0; i < col; i++) {
+    printf("%c", c);
+  }
+  printf("\n");
+  return 0;
+}
