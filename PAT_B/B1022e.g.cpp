0d64c347cbecf59d2ad8d1897968df62b4fbb37f
FunamiYui <46393174+FunamiYui@users.noreply.github.com>
2019-01-13 15:57:46 +0800

    Update B1022e.g.cpp

--- PAT_B/B1022e.g.cpp
+++ PAT_B/B1022e.g.cpp
@@ -6,11 +6,11 @@ int main() {
   scanf("%d%d%d", &a, &b, &d);
   int sum = a + b;
   int ans[40], num = 0;   //ans存放D进制结果的每一位，根据题目给的条件其实数组大小31就够用,num相当于数组指针
-  do {
+  do {            //进制转换
     ans[num++] = sum % d;
     sum /= d;
   } while(sum != 0);      //while（）后面的';'不要落下
-  for(int i = num - 1; i >= 0; i--) {
+  for(int i = num - 1; i >= 0; i--) {   //从高位向低位进行输出
     printf("%d", ans[i]);
   }
   return 0;
