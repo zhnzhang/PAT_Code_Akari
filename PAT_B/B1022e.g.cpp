ed33d68930733d15ecd26e10a0bde4aab95b42e0
FunamiYui <46393174+FunamiYui@users.noreply.github.com>
2019-01-13 15:56:36 +0800

    Create B1022e.g.cpp

---
+++ PAT_B/B1022e.g.cpp
@@ -0,0 +1,17 @@
+//example
+//采用除基取余法,循环部分采用do...while语句以处理初值为0时的特殊情况
+#include <cstdio>
+int main() {
+  int a, b, d;
+  scanf("%d%d%d", &a, &b, &d);
+  int sum = a + b;
+  int ans[40], num = 0;   //ans存放D进制结果的每一位，根据题目给的条件其实数组大小31就够用,num相当于数组指针
+  do {
+    ans[num++] = sum % d;
+    sum /= d;
+  } while(sum != 0);      //while（）后面的';'不要落下
+  for(int i = num - 1; i >= 0; i--) {
+    printf("%d", ans[i]);
+  }
+  return 0;
+}
