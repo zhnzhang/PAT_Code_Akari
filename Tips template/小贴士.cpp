5eb2bfbe9d520f86458da4fc5cfcdca36e2370c3
FunamiYui <46393174+FunamiYui@users.noreply.github.com>
2019-02-28 12:13:58 +0800

    Update 小贴士.cpp

--- Tips template/小贴士.cpp
+++ Tips template/小贴士.cpp
@@ -19,3 +19,6 @@ for(set<int>::iterator it = st.begin(); it != st.end(); it++) {     //遍历set
 
 //在scanf或者cin输入书的编号id后，必须用getchar接收掉id后面的换行，否则getline将会换行读入
 //(这里要注意，也就是scanf(接收完字符后面是换行的话)后面接getline的话，中间要加个getchar()收集换行，这是由scanf与getline的特性来决定的)
+
+
+//STL中，对queue，使用front()和pop()函数前，必须用empty()判断队列是否为空，否则可能因为队空而出现错误
