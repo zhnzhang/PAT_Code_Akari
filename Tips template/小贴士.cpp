e2213b9aa4af0e96f76bd589d6f2fad70335091b
FunamiYui <46393174+FunamiYui@users.noreply.github.com>
2019-02-28 10:57:48 +0800

    Update 小贴士.cpp

--- Tips template/小贴士.cpp
+++ Tips template/小贴士.cpp
@@ -13,4 +13,9 @@ for(set<int>::iterator it = st.begin(); it != st.end(); it++) {     //遍历set
 
 //通过map直接建立int至int的映射，相比开int数组的优势在于：以免数据范围过大，可能导致的内存超限。
 
+
 //字符串以及map的参数传递速度较慢，如果需要作为函数的参数的话需要尽可能加上引用
+
+
+//在scanf或者cin输入书的编号id后，必须用getchar接收掉id后面的换行，否则getline将会换行读入
+//(这里要注意，也就是scanf后面接getline的话，中间要加个getchar()收集换行，这是由scanf与getline的特性来决定的)
