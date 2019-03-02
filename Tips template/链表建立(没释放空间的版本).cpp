ae10cca76a03168832f9cc6b10041cbcf2234cdf
FunamiYui <46393174+FunamiYui@users.noreply.github.com>
2019-03-02 11:36:35 +0800

    Update 链表建立(没释放空间的版本).cpp

--- Tips template/链表建立(没释放空间的版本).cpp
+++ Tips template/链表建立(没释放空间的版本).cpp
@@ -4,32 +4,34 @@
 
 #include <cstdio>
 #include <cstdlib>
-struct node {
+struct node {   //链表结点
     int data;
     node* next;
 };
 
+//创建链表(关键函数)
 node* create(int Array[]) {
-    node *p, *pre, *head;
-    head = new node;
-    head->next = NULL;
-    pre = head;
+    node *p, *pre, *head;   //pre保存当前结点的前驱结点，head为头结点
+    head = new node;    //创建头结点
+    head->next = NULL;  //头结点不需要数据域，指针域初始为NULL
+    pre = head; //记录pre为head
     for(int i = 0; i < 5; i++) {
-        p = new node;
+        p = new node;   //新建结点
+        //将Array[i]赋给新建的结点作为数据域，也可以scanf输入
         p->data = Array[i];
-        p->next = NULL;
-        pre->next = p;
-        pre = p;
+        p->next = NULL; //新结点的指针域设为NULL
+        pre->next = p;  //前驱结点的指针域设为当前新建结点的地址
+        pre = p;    //把pre设为p，作为下个结点的前驱结点
     }
-    return head;
+    return head;    //返回头结点指针
 }
 
 int main() {
     int Array[5] = {5, 3, 6, 1, 2};
-    node* L = create(Array);
-    L = L->next;
+    node* L = create(Array);    //新建链表，返回的头指针head赋给L
+    L = L->next;    //从第一个结点开始有数据域
     while(L != NULL) {
-        printf("%d", L->data);
+        printf("%d", L->data);  //输出每个结点的数据域
         L = L->next;
     }
     return 0;
