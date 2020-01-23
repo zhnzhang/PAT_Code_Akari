76f8ee284e9b631eee6ee1572bf6e19088d42354
FunamiYui <46393174+FunamiYui@users.noreply.github.com>
2020-01-23 22:12:55 +0800

    Update and rename A1042|Shuffling Machine|e.g.cpp to A1042|Shuffling Machine|.cpp

--- PAT_A/A1042|Shuffling Machine|.cpp
+++ PAT_A/A1042|Shuffling Machine|.cpp
@@ -1,3 +1,37 @@
+#include <cstdio>     //my version
+
+int main() {
+    int k, temp = 0, order[60] = {0}, end[60] = {0};    //order数组放洗牌规则
+    char str[60][5] = {"SM",
+                       "S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10", "S11", "S12", "S13",
+                       "H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "H11", "H12", "H13",
+                       "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13",
+                       "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10", "D11", "D12", "D13",
+                       "J1", "J2"};             //由于下面的循环参数设置，这里的str[0]要用点什么填上
+    scanf("%d", &k);
+    for (int i = 1; i <= 54; i++) {
+        scanf("%d", &order[i]);
+    }
+    for (int i = 1; i <= 54; i++) {
+        temp = i;
+        for (int j = 1; j <= k; j++) {
+            temp = order[temp];                 //洗牌逻辑
+        }
+        end[i] = temp;                          //end[i]对应str[i]洗牌最后应该放置的位置，搞清楚这个十分重要，我就是卡在这里了。
+    }
+    for (int i = 1; i <= 54; i++) {             //对什么位置应放哪个字符串进行实现
+        for (int j = 1; j <= 54; j++) {
+            if (i == end[j]) {
+                printf("%s", str[j]);
+            }
+        }
+        if (i != 54) {
+            printf(" ");
+        }
+    }
+    return 0;
+}
+
 //example 我没写出来，参考答案如下
 #include <cstdio>
 const int N = 54;
