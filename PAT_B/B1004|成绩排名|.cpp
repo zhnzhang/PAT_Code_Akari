92598704c67d37a9f53ad6c98ca8a9b67944133d
FunamiYui <46393174+FunamiYui@users.noreply.github.com>
2020-01-29 18:28:31 +0800

    Update and rename B1004|成绩排名|e.g.cpp to B1004|成绩排名|.cpp

--- PAT_B/B1004|成绩排名|.cpp
+++ PAT_B/B1004|成绩排名|.cpp
@@ -1,3 +1,39 @@
+#include <cstdio>
+
+typedef struct student {
+    char name[15];
+    char id[15];
+    int score;
+}student;
+
+student stu[1010];
+
+int main() {
+    int n;
+    int max = -1, max_id = 0;               //max记录最高分，max_id记录最高分学生的存储在数组中的下标，反向设置初值
+    int min = 110, min_id = 0;              //min记录最低分，min_id记录最低分学生的存储在数组中的下标，反向设置初值
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%s %s %d", stu[i].name, stu[i].id, &stu[i].score);
+        if (stu[i].score > max) {
+            max = stu[i].score;             //相应的要更新
+            max_id = i;
+        }
+        if (stu[i].score < min) {
+            min = stu[i].score;
+            min_id = i;
+        }
+    }
+    printf("%s %s\n", stu[max_id].name, stu[max_id].id);
+    printf("%s %s\n", stu[min_id].name, stu[min_id].id);
+    return 0;
+}
+
+
+
+
+
+
 //Example
 //字符数组name和id的大小必须至少是11而不能设为10，否则第一个数据和最后一个数据会得到答案错误。
 //这是因为字符数组的最后一位需要预留给'\0'，所以数组大小必须比题目要求的大小至少大1位
