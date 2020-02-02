1d8358350322bad29ccb542310253af65fd80591
FunamiYui <46393174+FunamiYui@users.noreply.github.com>
2020-02-02 20:07:44 +0800

    Update and rename A1006|Sign In and Sign Out|e.g.cpp to A1006|Sign In and Sign Out|.cpp

--- PAT_A/A1006|Sign In and Sign Out|.cpp
+++ PAT_A/A1006|Sign In and Sign Out|.cpp
@@ -1,3 +1,42 @@
+#include <cstdio>
+
+typedef struct person {
+    char id[20];
+    int intime;                     //这里的intime和outtime都是改过样式的
+    int outtime;
+}person;
+
+person ps[10010];
+
+int ChangeStyle(int hh, int mm, int ss) {
+    int tmp = hh * 10000 + mm * 100 + ss;
+    return tmp;
+}
+
+int main() {
+    int m, hh, mm, ss;              //hh mm ss 用于暂存
+    int max_id = 0, min_id = 0, max = -1, min = 250000;     //max指签离最晚，min指签到最早（最重要的是反向设初值，这是最骚的）
+    scanf("%d", &m);
+    for (int i = 0; i < m; i++) {
+        scanf("%s %d:%d:%d ", ps[i].id, &hh, &mm, &ss);
+        ps[i].intime = ChangeStyle(hh, mm, ss);
+        scanf("%d:%d:%d", &hh, &mm, &ss);
+        ps[i].outtime = ChangeStyle(hh, mm, ss);
+        if (ps[i].intime < min) {
+            min = ps[i].intime;
+            min_id = i;
+        }
+        if (ps[i].outtime > max) {
+            max = ps[i].outtime;
+            max_id = i;
+        }
+    }
+    printf("%s %s\n", ps[min_id].id, ps[max_id].id);
+    return 0;
+}
+
+
+
 //example 边读边做
 #include <iostream>
 #include <cstdio>
