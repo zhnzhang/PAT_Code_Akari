877fe2eab4900d65b72178e9057b0e2236e41de3
FunamiYui <46393174+FunamiYui@users.noreply.github.com>
2020-02-04 19:17:35 +0800

    Update and rename B1037|在霍格沃茨找零钱|e.g.cpp to B1037|在霍格沃茨找零钱|.cpp

--- PAT_B/B1037|在霍格沃茨找零钱|.cpp
+++ PAT_B/B1037|在霍格沃茨找零钱|.cpp
@@ -1,3 +1,38 @@
+#include <cstdio>
+
+int abs(int x) {
+    if (x >= 0) {
+        return x;
+    } else {
+        return -x;
+    }
+}
+
+int MakeCoin(int g, int s, int k) {         //全换成零钱形式
+    return g * 17 * 29 + s * 29 + k;
+}
+
+int main() {
+    int p, a, sub, subabs, g, s, k;         //sub是指作差，subabs是对作差求绝对值
+    scanf("%d.%d.%d", &g, &s, &k);
+    p = MakeCoin(g, s, k);
+    scanf("%d.%d.%d", &g, &s, &k);
+    a = MakeCoin(g, s, k);
+    sub = a - p;
+    subabs = abs(sub);
+    k = subabs % 29;
+    g = subabs / (29 * 17);
+    s = (subabs - g * 29 * 17) / 29;
+    if (sub >= 0) {                         //这里必须>=，最后一个检测点考察这里0的情况
+        printf("%d.%d.%d\n", g, s, k);
+    } else {
+        printf("-%d.%d.%d\n", g, s, k);
+    }
+    return 0;
+}
+
+
+
 //example
 //获得Knut为单位的找零的钱change后，要将它取绝对值，才能进行后面的计算。不能直接把负数代入后续格式转换的计算
 #include <cstdio>
