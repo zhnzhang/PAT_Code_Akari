94809ebc625d99372e8d905b3da1c6ac0b78c0c4
FunamiYui <46393174+FunamiYui@users.noreply.github.com>
2020-02-04 23:36:08 +0800

    Update and rename A1058|A+B in Hogwarts|e.g.cpp to A1058|A+B in Hogwarts|.cpp

--- PAT_A/A1058|A+B in Hogwarts|.cpp
+++ PAT_A/A1058|A+B in Hogwarts|.cpp
@@ -1,3 +1,31 @@
+//我这种写法，在中间计算过程中可能会出现int的数据范围不够的情况，因为题中g的范围上界再加上它会乘上493
+//所以需要使用long long类型
+//事实证明是这样的，测试点2就是大数据，用int会溢出，导致答案错误
+//当然算法笔记那种按进位来运作的写法用不到
+
+#include <cstdio>
+#define ll long long
+
+ll MakeChange(ll g, ll s, ll k) {
+    return g * 29 * 17 + s * 29 + k;
+}
+
+int main() {
+    ll a, b, g, s, k, sum;
+    scanf("%lld.%lld.%lld", &g, &s, &k);
+    a = MakeChange(g, s, k);
+    scanf("%lld.%lld.%lld", &g, &s, &k);
+    b = MakeChange(g, s, k);
+    sum = a + b;
+    g = sum / (29 * 17);
+    s = sum % (29 * 17) / 29;
+    k = sum % 29;
+    printf("%lld.%lld.%lld\n", g, s, k);
+    return 0;
+}
+
+
+
 //example
 //说明有些冗长，具体说明见算法笔记
 //但这里需要用到一个变量去存储低位进位的数据，当然这里0号位没有进位限制，不需要取模或整除运算
