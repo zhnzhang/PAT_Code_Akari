2e2dfac4982183ddfd9bd64e7ac240d4fc569960
FunamiYui <46393174+FunamiYui@users.noreply.github.com>
2020-02-04 15:49:58 +0800

    Update and rename B1022|D进制的A+B|e.g.cpp to B1022|D进制的A+B|.cpp

--- PAT_B/B1022|D进制的A+B|.cpp
+++ PAT_B/B1022|D进制的A+B|.cpp
@@ -1,3 +1,25 @@
+//算法笔记P93介绍了进制转换的方法，总的来说还是以十进制作媒介的
+#include <cstdio>
+
+int main() {
+    int a, b, d, sum, i = 0;
+    int num[50];
+    scanf("%d %d %d", &a, &b, &d);
+    sum = a + b;
+    do {
+        num[i] = sum % d;
+        sum /= d;
+        i++;
+    } while (sum != 0);
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%d", num[j]);
+    }
+    printf("\n");
+    return 0;
+}
+
+
+
 //example
 //采用除基取余法,循环部分采用do...while语句以处理初值为0时的特殊情况
 //如果进制转换的时候使用的是while语句而不是do...while语句，则要注意当A + B为0时要特判 输出0
