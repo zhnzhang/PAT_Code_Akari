4c66e5faf27d2dbd8439299c99fae00fd16bebb3
FunamiYui <46393174+FunamiYui@users.noreply.github.com>
2020-02-05 16:27:47 +0800

    Update and rename B1006|换个格式输出整数|e.g.cpp to B1006|换个格式输出整数|.cpp

--- PAT_B/B1006|换个格式输出整数|.cpp
+++ PAT_B/B1006|换个格式输出整数|.cpp
@@ -1,3 +1,30 @@
+#include <cstdio>
+
+int main() {
+    int n, i = 0, a[5] = {0};
+    scanf("%d", &n);
+    if (n == 0) {
+        printf("0\n");
+        return 0;
+    }
+    while (n != 0) {
+        a[i++] = n % 10;
+        n /= 10;
+    }
+    for (int j = 0; j < a[2]; j++) {
+        printf("B");
+    }
+    for (int j = 0; j < a[1]; j++) {
+        printf("S");
+    }
+    for (int j = 1; j <= a[0]; j++) {
+        printf("%d", j);
+    }
+    return 0;
+}
+
+
+
 //example
 #include <cstdio>
 int main() {
