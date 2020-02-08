65585e649389274cc4504c4cf6e14b2a705638ea
FunamiYui <46393174+FunamiYui@users.noreply.github.com>
2020-02-08 16:57:09 +0800

    Update and rename B1048|数字加密|e.g.cpp to B1048|数字加密|.cpp

--- PAT_B/B1048|数字加密|.cpp
+++ PAT_B/B1048|数字加密|.cpp
@@ -1,3 +1,61 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+void reverse(char str[]) {                  //反转字符串
+    int len = strlen(str);
+    char tmp;
+    for (int i = 0; i < len / 2; i++) {
+        tmp = str[i];
+        str[i] = str[len - 1 - i];
+        str[len - 1 - i] = tmp;
+    }
+}
+
+int main() {
+    int lena = 0, lenb = 0, len = 0;
+    char a[110], b[110], ans[110];
+    scanf("%s %s", a, b);
+    reverse(a);
+    reverse(b);                 //反转后由于从0号位开始，题意中的奇偶数位处理方法要交换一下才能对得上
+    lena = strlen(a);
+    lenb = strlen(b);
+    len = lena > lenb ? lena : lenb;
+    fill(a + lena, a + 102, '0');       //用于多出的位数的处理
+    fill(b + lenb, b + 102, '0');
+    fill(ans + len, ans + 102, '\0');   //放好初值以防干扰
+    for (int i = 0; i < len; i++) {
+        int numa = a[i] - '0';
+        int numb = b[i] - '0';
+        int sum = 0;
+        if (i % 2 == 0) {       //由于反转，用奇数位处理方法
+            sum = numa + numb;
+            sum %= 13;
+            if (sum == 10) {
+                ans[i] = 'J';
+            } else if (sum == 11) {
+                ans[i] = 'Q';
+            } else if (sum == 12) {
+                ans[i] = 'K';
+            } else {
+                ans[i] = '0' + sum;
+            }
+        } else {                //由于反转，用偶数位处理方法
+            if (numb - numa < 0) {
+                ans[i] = '0' + (numb - numa + 10);
+            } else {
+                ans[i] = '0' + (numb - numa);
+            }
+        }
+    }
+    reverse(ans);       //再反转一次才能回归原位
+    printf("%s\n", ans);
+    return 0;
+}
+
+
+
 //example
 //为方便整数对齐和让最低位从数组0号开始，首先将字符串反转,处理好加密后，输出时，应将char型数组ans[]再反转进行输出
 //因为反转后的情况且字符串下标从0开始，要根据题设有针对性的调整
