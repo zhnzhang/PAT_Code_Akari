b364a54078b7fb3a9056e70f10c166d62fbd26de
FunamiYui <46393174+FunamiYui@users.noreply.github.com>
2020-02-08 23:49:13 +0800

    Update and rename A1001|A+B Format|e.g.cpp to A1001|A+B Format|.cpp

--- PAT_A/A1001|A+B Format|.cpp
+++ PAT_A/A1001|A+B Format|.cpp
@@ -1,3 +1,66 @@
+//题意是想这样的呈现 1,555   1,777,776
+
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int a, b, i = 0, len = 0, sum;  //i用于取位时的循环计数，len是和的位数
+    char ans[15];
+    fill(ans, ans + 15, '\0');      //这步还蛮重要的，不赋上初值容易随机出现一些奇怪的答案
+    scanf("%d %d",&a, &b);
+    sum = a + b;
+    if (sum < 0) {
+        printf("-");
+        sum = sum * -1;
+    }
+    do {
+        int tmp = sum % 10;
+        ans[i++] = '0' + tmp;
+        sum /= 10;
+    } while (sum != 0);
+    len = strlen(ans);
+    reverse(ans, ans + len);
+    if (len <= 3) {                         //不用逗号
+        for (int j = 0; j < len; j++) {
+            printf("%c", ans[j]);
+        }
+    } else {
+        if (len % 3 == 0) {                 //举个实例，再推广成规律
+            for (int j = 0; j < len; j++) {
+                printf("%c", ans[j]);
+                if (j % 3 == 2 && j != len - 1) {
+                    printf(",");
+                }
+            }
+        } else if (len % 3 == 1) {
+            for (int j = 0; j < len; j++) {
+                printf("%c", ans[j]);
+                if (j == 0) {
+                    printf(",");
+                }
+                if ((j - 1) % 3 == 2 && j != len - 1) {
+                    printf(",");
+                }
+            }
+        } else {
+            for (int j = 0; j < len; j++) {
+                printf("%c", ans[j]);
+                if (j == 1) {
+                    printf(",");
+                }
+                if ((j - 2) % 3 == 2 && j != len - 1) {
+                    printf(",");
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+
+
 //example
 //说明比较冗长，见算法笔记习题集P73-74
 //正常解法 两个步骤，步骤1不能省，步骤2在简洁写法里可以略去。其实我感觉这个参考答案的变量名的设置不怎么合我的习惯，结果数组我还是习惯ans，数组指针用num表示
