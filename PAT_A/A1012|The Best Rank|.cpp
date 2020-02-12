a63d3ffd70c23fc397f50496018c9bf9102e0a3d
FunamiYui <46393174+FunamiYui@users.noreply.github.com>
2020-02-12 21:55:09 +0800

    Update and rename A1012|The Best Rank|e.g.cpp to A1012|The Best Rank|.cpp

--- PAT_A/A1012|The Best Rank|.cpp
+++ PAT_A/A1012|The Best Rank|.cpp
@@ -1,3 +1,70 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+typedef struct student {
+    char id[10];
+    int score[5];             //平均分是四舍五入的，这里我用总分替代，因为线性相关的，所以效果是一样的
+    int rank[5];              //顺序就按a c m e 对 0 1 2 3吧
+}student;
+
+student stu[2020];
+int now;                        //全局变量，用于数据的同步处理!重要的点
+char mp[5] = {'A', 'C', 'M', 'E'};
+
+bool cmp(student a, student b) {
+    return a.score[now] > b.score[now];
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < n; i++) {
+        scanf("%s %d %d %d", stu[i].id, &stu[i].score[1], &stu[i].score[2], &stu[i].score[3]);
+        stu[i].score[0] = stu[i].score[1] + stu[i].score[2] + stu[i].score[3];
+    }
+    for (now = 0; now < 4; now++) {
+        sort(stu, stu + n, cmp);
+        stu[0].rank[now] = 1;
+        for (int i = 1; i < n; i++) {           //常用的作排名的方法，现成的
+            if (stu[i].score[now] == stu[i - 1].score[now]) {
+                stu[i].rank[now] = stu[i - 1].rank[now];
+            } else {
+                stu[i].rank[now] = i + 1;
+            }
+        }
+    }
+    for (int i = 0; i < m; i++) {
+        char str[10];
+        bool flag = false;  //标记是否找到
+        scanf("%s", str);
+        for (int j = 0; j < n; j++) {
+            if (!strcmp(str, stu[j].id)) { //这里特别标注：要做字符串的比较最好用strcmp，我直接用==就做不出答案，可能因为没有初始化
+                flag = true;
+                int min = n + 1;            //最靠前的名次，因为我的顺序是排好的，所以逻辑上没有问题
+                int u;                      //用来记录最考前的名次是哪种分数对应的
+                for (int k = 0; k < 4; k++) {
+                    if (stu[j].rank[k] < min) {         //得是 < 才行
+                        min = stu[j].rank[k];
+                        u = k;
+                    }
+                }
+                printf("%d %c\n", min, mp[u]);
+                break;
+            }
+        }
+        if (flag == false) {
+            printf("N/A\n");
+        }
+    }
+    return 0;
+}
+
+
+
+
+
 //example
 //相关数据结构要设置好
 #include <iostream>
